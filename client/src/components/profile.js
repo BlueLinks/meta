247822dbb07fe2a3ec52e45da01e34994bb34cfc
BlueLinks <scott_m_brown@mac.com>
2022-12-19 21:15:36 +0000

    Closes #8, client dir in root has been removed.

--- client/src/components/profile.js
+++
@@ -1,9 +0,0 @@
-export default function ProfilePage() {
-	return (
-		<div className="container-sm mt-3">
-			<div className="mt-4 p-3 bg-dark text-white rounded">
-				<h1>Hello</h1>
-			</div>
-		</div>
-	);
-}
